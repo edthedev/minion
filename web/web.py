d4f02c861f49fb771f926a5ba31a006da7e74f2a
Edward Delaporte <edthedev@gmail.com>
2014-11-08 20:50:44 -0600

    Working example.

--- web/web.py
+++ web/web.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def today():
     context = {}
-    context['list'] = ['Hello', 'World']
+    context['files'] = ['Hello', 'World']
     return render_template('today.html', **context)
 
 if __name__ == '__main__':
