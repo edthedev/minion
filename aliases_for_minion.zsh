452b1c868a355f90f8410cd66efd7c4dfe850b63
Edward Delaporte <edthedev@gmail.com>
2015-02-28 17:10:20 -0600

    Some cleanup

--- aliases_for_minion.zsh
+++ aliases_for_minion.zsh
@@ -7,9 +7,11 @@ alias minion="minion.py"
 alias mn="minion.py"
 
 # Let's add content.
-alias journal="minion.py template journal $@"
 alias remind="minion.py remind $@"
 alias rem="minion.py remind $@"
+
+# Some templates
+alias journal="minion.py template journal $@"
 alias week="minion.py template week"
 alias weekend="minion.py template weekend"
 
@@ -19,4 +21,4 @@ alias today="minion.py find urgent today `date +%Y-%m-%d` goal `date +%A`"
 alias where_in_the_world_is="minion.py find --archives $@"
 
 # For development under Zshell
-alias testminion="pushd ~/.vim/bundle/minion/tests; ./run_all_tests.sh; popd"
+# alias testminion="pushd ~/.vim/bundle/minion/tests; ./run_all_tests.sh; popd"
