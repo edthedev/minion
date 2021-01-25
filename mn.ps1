021e6e94fb1b485b620b9a8473793603b78d50c7
Edward Delaporte <edward@delaporte.us>
2021-01-25 15:30:33 -0600

    New note for today, in PowerShell

--- mn.ps1
+++ mn.ps1
@@ -1,3 +1,23 @@
 #!/usr/bin/pwsh
-$dateFile = Get-Date -Format "yyyy/MM/dd"
-Write-Output "~/Journal/$dateFile.md"
+
+<#
+.EXAMPLE
+
+In PowerShell to start a new note for today:
+
+> vim $(~/src/minion/mn.ps1)
+
+#>
+
+$daySlug = Get-Date -Format "dd"
+$ymSlug = Get-Date -Format "yyyy/MM"
+$folder = "~/Journal/$ymSlug"
+$fileName = "$folder/$daySlug.md"
+
+if(-Not(Test-Path -Path $folder)) {
+	$_ = New-Item -Type directory -path $folder -Force
+}
+if(-Not(Test-Path -Path $fileName)) {
+	$_ = New-Item -Type file -path $fileName -Force
+}
+Write-Output "$fileName"
