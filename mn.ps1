0a5fc880edd9b87af9abbc214d75670b0e0d1d88
Edward Delaporte <edward@delaporte.us>
2021-01-25 15:37:49 -0600

    Params

--- mn.ps1
+++ mn.ps1
@@ -1,23 +1,47 @@
 #!/usr/bin/pwsh
 
 <#
+.SYNOPSIS
+
+Quick script to help with rapid journaling.
+
 .EXAMPLE
 
 In PowerShell to start a new note for today:
 
-> vim $(~/src/minion/mn.ps1)
+> vim $(~/src/minion/mn.ps1 -today)
+
+.NOTES
+
+Recommended for your PowerShell profile:
+
+> New-Alias mn ~/src/minion/mn.ps1
+
+.EXAMPLE
+
+> vim $(mn today)
+
 
 #>
+param(
+	[switch]$today = $false
+)
 
-$daySlug = Get-Date -Format "dd"
-$ymSlug = Get-Date -Format "yyyy/MM"
-$folder = "~/Journal/$ymSlug"
-$fileName = "$folder/$daySlug.md"
+function JournalToday() {
+	$daySlug = Get-Date -Format "dd"
+	$ymSlug = Get-Date -Format "yyyy/MM"
+	$folder = "~/Journal/$ymSlug"
+	$fileName = "$folder/$daySlug.md"
 
-if(-Not(Test-Path -Path $folder)) {
-	$_ = New-Item -Type directory -path $folder -Force
+	if(-Not(Test-Path -Path $folder)) {
+		$_ = New-Item -Type directory -path $folder -Force
+	}
+	if(-Not(Test-Path -Path $fileName)) {
+		$_ = New-Item -Type file -path $fileName -Force
+	}
+	Write-Output "$fileName"
 }
-if(-Not(Test-Path -Path $fileName)) {
-	$_ = New-Item -Type file -path $fileName -Force
+
+if($today){
+	JournalToday
 }
-Write-Output "$fileName"
