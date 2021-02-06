799ed848fce6708767290a915ef8fc4487ba84ba
EdTheDev <edward@delaporte.us>
2021-02-05 22:10:00 -0600

    DocOpt for go is a bit rubbish.

--- go/minion.go
+++ go/minion.go
@@ -1,45 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/docopt/docopt-go"
-	"io/ioutil"
+	// "io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
 
 func main() {
-	usage := `Minion. Your command line personal assistance.
 
-Usage:
-	minion tags - list all tags
-	minion list - list all files matching keywords
-	minion find <text> - list all files matching keywords
-	minion -h | --help
-  minion --version
+	listFlag := flag.Bool("list", false, "List the files.")
+	flag.Parse()
 
-Options:
-  -h --help     Show this screen.
-  --version     Show version.
-	-d --days=<days>         Show notes modified last N days .
-	-m --max=<max>           Maximum results to display. [default: 10]
-	-y --year=<year>         Limit results to those created in the given year. [default: current year]`
+	fmt.Printf("List: %s, Find: %s", *listFlag, "...garbage...")
 
-	var conf struct {
-		Tags bool
-		List bool
-		Find []string
-		Max  int
-		Year int
-		Days int
-	}
-	opts, _ := docopt.ParseDoc(usage)
-	// fmt.Println(arguments)
-
-	opts.Bind(&conf)
-
-	if conf.List {
+	if *listFlag {
 		var err error
 		err = filepath.Walk("c:\\src\\minion", func(path string, info os.FileInfo, err error) error {
 			println(info.Name())
@@ -50,8 +27,7 @@ Options:
 		}
 	}
 
-	if conf.Tags {
-
+	/*
 		files, err := ioutil.ReadDir("c:\\src\\minion")
 		if err != nil {
 			log.Fatal(err)
@@ -60,6 +36,6 @@ Options:
 		for _, f := range files {
 			fmt.Println(f.Name())
 		}
-	}
+	*/
 
 }
