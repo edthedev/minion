cb377cdc8a4f29cebd6639b79d53095ffa90261c
Edward Delaporte <delaport@illinois.edu>
2021-02-09 12:51:18 -0600

    Porotyped 'tomorrow' command. - Need to clean up into PSM1.

--- bin/mn.ps1
+++ bin/mn.ps1
@@ -25,6 +25,7 @@ Recommended for your PowerShell profile:
 #>
 param(
 	[switch]$today = $false,
+	[switch]$tomorrow = $false,
 	[string]$title
 )
 
@@ -40,12 +41,33 @@ $today_template = @'
 '@
 $today_body = $today_template -f $prettyDate
 
-function JournalToday() {
-	$daySlug = Get-Date -Format "dd"
-	$ymSlug = Get-Date -Format "yyyy/MM"
+function Get-JournalFile() {
+	param(
+		[DateTime]$date
+	)
+  $daySlug = '{0:dd}' -f $date
+  $ymSlug = '{0:yyyy/MM}' -f $date
 	$folder = "~/Journal/$ymSlug"
 	$fileName = "$folder/$daySlug.md"
 
+	New-JournalFile -folder $folder -fileName $fileName
+
+	return $fileName
+
+}
+
+<#
+.SYNOPSIS
+
+Create journal file and folder path, even if it does not exist.
+Add template content if it does not already exist.
+
+#>
+function New-JournalFile() {
+	param(
+	[string]$folder,
+	[string]$fileName
+	)
 	if(-Not(Test-Path -Path $folder)) {
 		$_ = New-Item -Type directory -path $folder -Force
 	}
@@ -53,12 +75,40 @@ function JournalToday() {
 		$_ = New-Item -Type file -path $fileName -Force
 		Add-Content -Path $fileName -Value $today_body
 	}
+}
+
+<#
+.SYNOPSIS
+
+Open journal file for today.
+
+#>
+function JournalToday() {
+	$date = Get-Date -f 'yyyy-MM-dd'
+	$fileName = Get-JournalFile -Date $date
 	Write-Output "$fileName"
 }
+
+<#
+.SYNOPSIS
+
+Open journal file for tomrrow.
+
+#>
+function JournalTomorrow() {
+	$date = (Get-Date).AddDays(1) -f 'yyyy-MM-dd'
+	$fileName = Get-JournalFile -Date $date
+	Write-Output "$fileName"
+}
+
 
 if($today){
 	JournalToday
 }
+if($tomorrow){
+	JournalTomorrow
+}
+
 
 if($title){
 	$ymSlug = Get-Date -Format "yyyy/MM/dd"
