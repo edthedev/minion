598d981a92967f9e5be5c18723559926459800f7
Edward Delaporte <delaport@illinois.edu>
2021-02-10 17:17:18 -0600

    Started on --tags

--- go/minion.go
+++ go/minion.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"regexp"
 )
 
 func searchInFile(fileName string, searchString string) bool {
@@ -25,20 +26,76 @@ func searchInFile(fileName string, searchString string) bool {
 
 }
 
+
+func searchForTags(tagRegex regexp.Regexp, fileName string) bool {
+	var found bool
+	
+
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Printf("Unable to read file: %s - %s", fileName, err)
+	}
+	fileContent := string(data)
+
+	fmt.Println(tagRegex.FindAllString(fileContent, -1))
+
+	found = strings.Contains(fileContent, "NEVERMIND")
+	return found
+
+}
+
+
 func main() {
 
 	searchFlag := flag.String("search", "", "Text to search for.")
-	// listFlag := flag.Bool("list", false, "List the files.")
-	maxFlag := flag.Int("max", 5, "Maximum number of files to list.")
+	listFlag := flag.Bool("tags", false, "List any tags that look like [<text>].")
+	maxFlag := flag.Int("max", 5, "Maximum number of files or tags to list.")
 	flag.Parse()
+	tagRegex, _ := regexp.Compile("p([a-z]+)ch")
 
 	var rootPath string = `C:\Users\delaport\Journal\2021`
 	var notMarkdown int = 0
 	var matchCount int = 0
 
-	// fmt.Printf("List: %t, Find: %s.", *listFlag, *searchFlag)
-	fmt.Printf("List: %t, Find: %s.", "--default to list--", *searchFlag)
+	fmt.Printf("List: %t, Find: %s.", *listFlag, *searchFlag)
+	// fmt.Printf("List: %t, Find: %s.", "--default to list--", *searchFlag)
 	fmt.Println("")
+	if ( *listFlag ) {
+		var results = []string{}
+
+		walkErr := filepath.Walk(rootPath, func(path string, info os.FileInfo, err error) error {
+
+			if filepath.Ext(path) != ".md" {
+				notMarkdown += 1
+				return nil
+			}
+
+			found := searchForTags(*tagRegex, path)
+			if found {
+				// fmt.Printf("Found a match in: %s", path)
+				matchCount += 1
+				results = append(results, path)
+			} else {
+				// fmt.Printf("Found no match in: %s", path)
+			}
+
+			return nil
+		})
+		if walkErr != nil {
+			log.Fatal(walkErr)
+		}
+		fmt.Printf("Skipped %d non markdown (.md) files. ", notMarkdown)
+
+		if(matchCount > *maxFlag) {
+			fmt.Printf("Found %d matches.", matchCount)
+		} else {
+		  for _, item := range results {
+				fmt.Println(item)
+			}
+			// fmt.Println(results)
+		}
+
+	}
 
 	if (*searchFlag != "") {
 		var results = []string{}
